a83695f137d18c1125837e53f1cc92b3496f4083
bsy3764 <bsy3764@gmail.com>
2024-12-21 18:51:38 +0900

    Init commit

---
+++ src/test/java/com/lecture/architecture/ArchitectureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lecture.architecture;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArchitectureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
